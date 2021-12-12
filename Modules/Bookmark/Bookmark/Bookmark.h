02e762792d18f21057fe66f1e0a160dbc34587b5
Budi Darmawan <46107627+Budi77Darmawan@users.noreply.github.com>
2021-12-12 10:08:15 +0700

    add bookmark module

---
+++ Modules/Bookmark/Bookmark/Bookmark.h
@@ -0,0 +1,18 @@
+//
+//  Bookmark.h
+//  Bookmark
+//
+//  Created by Budi Darmawan on 12/12/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Bookmark.
+FOUNDATION_EXPORT double BookmarkVersionNumber;
+
+//! Project version string for Bookmark.
+FOUNDATION_EXPORT const unsigned char BookmarkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bookmark/PublicHeader.h>
+
+
